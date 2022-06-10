2a04d3d0e80a5bc8294077f715686627b8af75b5
glebite <glebite@gmail.com>
2022-06-09 20:35:14 -0400

    Thinking about the inputs...

--- src/user_commands.py
+++ src/user_commands.py
@@ -6,6 +6,12 @@ class UserInput:
     def __init__(self):
         pass
 
+    def input_date(self):
+        pass
+
+    def input_resource(self):
+        pass
+
 
 if __name__ == "__main__":
     pass
