b96e54375326cb45f92c2672710c19dced105bf7
Lazlo Klemenski <glebite@gmail.com>
2022-06-18 09:04:18 -0400

    Touching base while on vacation - updating parameters

--- src/user_commands.py
+++ src/user_commands.py
@@ -3,13 +3,21 @@
 
 
 class UserInput:
+    """
+    """
     def __init__(self):
+        """__init__
+        """
         pass
 
-    def input_date(self):
+    def input_date(self, date_input):
+        """input_date
+        """
         pass
 
-    def input_resource(self):
+    def input_resource(self, resource_id):
+        """input_resource - 
+        """
         pass
 
 
